99b599376d548b81bf825392aa97e405e67635bf
jcrout <jcrout127@yahoo.com>
2015-09-29 23:53:47 -0400

    Added tests to cover new feature:: getting a number from a string representing roman numerals. Code will not yet compile.
    The method should make use of NullReference and Argument exceptions when dealing with null/empty strings respectively. The method should also throw ArgumentException when strings contain non-numeral characters or when lowercase symbol characters are used. Subtractive notation should be enforced and throw ArgumentException on violations. Finally, the same number range between 1 and 3000 should be enforced on the return values and throw IndexOutOfRangeException on violations.

--- TDD-Samples.UnitTests/RomanNumeralConverterTests.cs
+++ TDD-Samples.UnitTests/RomanNumeralConverterTests.cs
@@ -14,7 +14,9 @@ namespace TDD_Samples.UnitTests
     {
         [TestCase(1, "I")]
         [TestCase(6, "VI")]
+        [TestCase(101, "CI")]
         [TestCase(2489, "MMCDLXXXIX")]
+        [TestCase(2644, "MMDCXLIV")]
         [TestCase(3000, "MMM")]
         public void GetRomanNumeral_NumberBetween1And3000_ReturnsCorrectRomanNumeral(int numberToConvert, string expectedRomanNumeralString)
         {
@@ -41,6 +43,76 @@ namespace TDD_Samples.UnitTests
             var invalidIndexAbove3000 = 3001;
 
             Assert.Throws<IndexOutOfRangeException>(() => converter.GetRomanNumeral(invalidIndexAbove3000));
+        }
+
+
+        [Test]
+        public void GetNumber_NullString_ThrowsArgumentNullException()
+        {
+            var converter = GetRomanNumeralConverter();
+
+            Assert.Throws<ArgumentNullException>(() => converter.GetNumber(null));
+        }
+
+        [Test]
+        public void GetNumber_EmptyString_ThrowsArgumentException()
+        {
+            var converter = GetRomanNumeralConverter();
+
+            Assert.Throws<ArgumentException>(() => converter.GetNumber(String.Empty));
+        }
+        
+        [TestCase("abc")]
+        [TestCase("IVR")]
+        [TestCase("MM ")]
+        [TestCase("O_O")]
+        public void GetNumber_StringContainingInvalidCharacters_ThrowsArgumentException(string invalidInput)
+        {
+            var converter = GetRomanNumeralConverter();
+
+            Assert.Throws<ArgumentException>(() => converter.GetNumber(invalidInput));
+        }
+
+        public void GetNumber_StringContainingLowerCaseSymbols_ThrowsArgumentException()
+        {
+            var converter = GetRomanNumeralConverter();
+            var invalidStringWithLowerCaseSymbols = "mmm";
+
+            Assert.Throws<ArgumentException>(() => converter.GetNumber(invalidStringWithLowerCaseSymbols));
+        }
+
+        [TestCase("IIII")]
+        [TestCase("XXXXX")]
+        [TestCase("MCCCC")]
+        public void GetNumber_InvalidSubtractiveNotation_ThrowsArgumentException(string invalidSubtractiveNotation)
+        {
+            var converter = GetRomanNumeralConverter();
+
+            Assert.Throws<ArgumentException>(() => converter.GetNumber(invalidSubtractiveNotation));
+        }
+
+        [Test]
+        public void GetNumber_NumeralAbove3000_ThrowsIndexOutOfRange()
+        {
+            var converter = GetRomanNumeralConverter();
+            var number3002 = "MMMII";
+
+            Assert.Throws<IndexOutOfRangeException>(() => converter.GetNumber(number3002));
+        }
+
+        [TestCase("I", 1)]
+        [TestCase("VI", 6)]
+        [TestCase("CI", 101)]
+        [TestCase("MMCDLXXXIX", 2489)]
+        [TestCase("MMDCXLIV", 2644)]
+        [TestCase("MMM", 3000)]
+        public void GetNumber_NumeralBetween1And3000_ReturnsCorrectNumber(string validInput, int expectedResult)
+        {
+            var converter = GetRomanNumeralConverter();
+
+            var result = converter.GetNumber(validInput);
+
+            Assert.AreEqual(expectedResult, result);
         }
 
         public RomanNumeralConverter GetRomanNumeralConverter()
