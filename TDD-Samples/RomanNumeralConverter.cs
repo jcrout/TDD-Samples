6094ff6b360ceee2cbea4dcda15f3cc3239ebc9b
jcrout <jcrout127@yahoo.com>
2015-09-29 23:12:56 -0400

    Added the bare-minimum production code needed to compile. All tests fail. Added package NUnitTestAdapter

---
+++ TDD-Samples/RomanNumeralConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_Samples
+{
+    public class RomanNumeralConverter
+    {
+        public string GetRomanNumeral(int numberToConvert)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
